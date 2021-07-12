1aa25bc3323a557f99e2858e940b8c7fef9615f7
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-07-12 18:22:56 -0300

    move: entry_repostory to repositories folder

--- app/entry/service.py
+++ app/entry/service.py
@@ -1,35 +1,35 @@
 from app.entry.entry import Entry
 from app.entry.models import EntryCreation, EntryUpdate
-from app.entry import repository as EntryReposiory
+from app.entry.repositories import entry_repository
 
 
 class EntryService:
     @staticmethod
     def get_all(domain: str) -> list[Entry]:
-        ok, entries = EntryReposiory.get_all(domain)
+        ok, entries = entry_repository.get_all(domain)
         return entries
 
     @staticmethod
     def get_one(domain: str, group: str, title: str) -> Entry:
-        ok, entry = EntryReposiory.get_one(
+        ok, entry = entry_repository.get_one(
             domain=domain, group=group, title=title
         )
         return entry
 
     @staticmethod
     def get(id: str) -> Entry:
-        ok, entry = EntryReposiory.get(id=id)
+        ok, entry = entry_repository.get(id=id)
         return entry
 
     @staticmethod
     def save(entry: EntryCreation) -> Entry:
-        result = EntryReposiory.save(entry)
+        result = entry_repository.save(entry)
         return result.inserted_id
 
     @staticmethod
     def delete(id: str):
-        EntryReposiory.delete(id=id)
+        entry_repository.delete(id=id)
 
     @staticmethod
     def update(id: str, entry: EntryUpdate):
-        EntryReposiory.update(id, entry)
+        entry_repository.update(id, entry)
